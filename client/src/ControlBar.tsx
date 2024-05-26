fc3442f9c3182655b6c747d9a6d121e9e07181eb
Luke Walker <lukeryanwalker2000@gmail.com>
2024-05-25 22:19:58 -0700

    Added n input

--- client/src/ControlBar.tsx
+++ client/src/ControlBar.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
 import Select from "react-select"
-import { API_URL } from "../config/config.ts"
+import { API_URL } from "../config/config"
 
 export default function ControlBar() {
-    const [algs, setAlgs] = useState([]);
+    const [algs, setAlgs] = useState<string[]>([]);
+    const [n, setN] = useState<string>("");
 
     useEffect(() => {
         fetch(API_URL+"/list")
@@ -11,10 +12,26 @@ export default function ControlBar() {
             .then((data) => setAlgs(data));
     }, []);
 
+    function handleNChange(e: any) {
+        if (isNaN(e.target.value) && e.target.value !== "") {
+            return;
+        }
+
+        setN(e.target.value);
+    }
+
     return (
         <div className="control-bar">
-            <div className="algorithm-select">
-                <Select options={algs.map(alg => ({value: alg, label: alg}))} />
+            <div className="algorithm-form">
+                <div className="algorithm-select">
+                    <Select options={algs.map(alg => ({value: alg, label: alg}))} />
+                </div>
+                <div className="algorithm-n">
+                    <input type="text" placeholder="n" value={n} onChange={handleNChange} />
+                </div>
+                <div className="algorithm-submit">
+
+                </div>
             </div>
         </div>
     );
