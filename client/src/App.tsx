ed9b882026709abc2602159b9d21ea6499431e0f
Luke Walker <lukeryanwalker2000@gmail.com>
2024-05-25 23:14:44 -0700

    Added visualizer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ControlBar from "./ControlBar"
 import Visualizer from "./Visualizer"
 
 export default function App() {
     const [data, setData] = useState<number[][]>([]);
+    const [step, setStep] = useState<number[]>([]);
+
+    useEffect(() => {
+        if (data.length > 0) {
+            setStep(data[0]);
+        }
+    }, [data]);
 
     return (
         <div className="app">
-            <Visualizer />
+            <Visualizer step={step} />
             <ControlBar setData={setData} />
         </div>
     );
--- client/src/Visualizer.tsx
+++ client/src/Visualizer.tsx
@@ -1,7 +1,11 @@
-export default function Visualizer() {
+import "./Visualizer.css"
+
+export default function Visualizer({ step } : { step: number[] }) {
     return (
         <div className="visualizer">
-
+            {step.length > 0 && step.map((num) =>
+                <div className="data-bar" key={num} style={{height: num}} />
+            )}
         </div>
-    )
+    );
 }
