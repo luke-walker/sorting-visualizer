a638925b97d0a983a9763e108d3b5f2c3132fd2f
Luke Walker <lukeryanwalker2000@gmail.com>
2024-05-25 17:42:59 -0700

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import ControlBar from './ControlBar'
+import Visualizer from './Visualizer'
+
+export default function App() {
+    return (
+        <div className="app">
+            <Visualizer />
+            <ControlBar />
+        </div>
+    )
+}
---
+++ client/src/ControlBar.tsx
@@ -0,0 +1,7 @@
+export default function ControlBar() {
+    return (
+        <div className="control-bar">
+            
+        </div>
+    )
+}
