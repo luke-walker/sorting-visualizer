17d5a8f85e5fc9abf786f85aaaaef7a26aeb6722
Luke Walker <lukeryanwalker2000@gmail.com>
2024-05-25 19:20:08 -0700

    Added algorithm selection list

--- client/src/ControlBar.tsx
+++ client/src/ControlBar.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
+import Select from "react-select"
+import { API_URL } from "../config/config.ts"
+
 export default function ControlBar() {
+    const [algs, setAlgs] = useState([]);
+
+    useEffect(() => {
+        fetch(API_URL+"/list")
+            .then((res) => res.json())
+            .then((data) => setAlgs(data));
+    }, []);
+
     return (
         <div className="control-bar">
-            
+            <div className="algorithm-select">
+                <Select options={algs.map(alg => ({value: alg, label: alg}))} />
+            </div>
         </div>
-    )
+    );
 }
