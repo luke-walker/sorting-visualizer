635a794d4c3e0163123e9cfff4ff1dd420a10390
Luke Walker <lukeryanwalker2000@gmail.com>
2024-05-25 22:51:38 -0700

    Retrieve sorting data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
-import ControlBar from './ControlBar'
-import Visualizer from './Visualizer'
+import { useState } from "react"
+import ControlBar from "./ControlBar"
+import Visualizer from "./Visualizer"
 
 export default function App() {
+    const [data, setData] = useState<number[][]>([]);
+
     return (
         <div className="app">
             <Visualizer />
-            <ControlBar />
+            <ControlBar setData={setData} />
         </div>
-    )
+    );
 }
--- client/src/ControlBar.tsx
+++ client/src/ControlBar.tsx
@@ -1,16 +1,21 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, Dispatch, SetStateAction } from "react"
 import Select from "react-select"
 import { API_URL } from "../config/config"
 
-export default function ControlBar() {
-    const [algs, setAlgs] = useState<string[]>([]);
+export default function ControlBar({ setData } : { setData: Dispatch<SetStateAction<number[][]>> } ) {
+    const [algList, setAlgList] = useState<string[]>([]);
+    const [alg, setAlg] = useState<string>("");
     const [n, setN] = useState<string>("");
 
     useEffect(() => {
         fetch(API_URL+"/list")
             .then((res) => res.json())
-            .then((data) => setAlgs(data));
+            .then((data) => setAlgList(data));
     }, []);
+
+    function handleAlgChange(e: any) {
+        setAlg(e.value);
+    }
 
     function handleNChange(e: any) {
         if (isNaN(e.target.value) && e.target.value !== "") {
@@ -20,17 +25,24 @@ export default function ControlBar() {
         setN(e.target.value);
     }
 
+    function handleSubmitClick() {
+        fetch(`${API_URL}/sort/${alg}/${n}`)
+            .then((res) => res.json())
+            .then((data) => setData(data));
+    }
+
     return (
         <div className="control-bar">
             <div className="algorithm-form">
                 <div className="algorithm-select">
-                    <Select options={algs.map(alg => ({value: alg, label: alg}))} />
+                    <Select placeholder="Select algorithm" options={algList.map(x => ({value: x, label: x}))} 
+                        onChange={handleAlgChange} />
                 </div>
                 <div className="algorithm-n">
                     <input type="text" placeholder="n" value={n} onChange={handleNChange} />
                 </div>
                 <div className="algorithm-submit">
-
+                    <button onClick={handleSubmitClick}>Submit</button>
                 </div>
             </div>
         </div>
