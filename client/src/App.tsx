1ac3b5f5c105591b86860f68bc30a7d526999206
Luke Walker <lukeryanwalker2000@gmail.com>
2024-05-26 21:00:04 -0700

    Moved everything to App.tsx
    Too many impurities

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,124 @@
 import { useEffect, useState } from "react"
-import ControlBar from "./ControlBar"
-import Visualizer from "./Visualizer"
+import Select from "react-select"
+import { API_URL } from "../config/config"
+import "./App.css"
 
 export default function App() {
-    const [data, setData] = useState<number[][]>([]);
+    const [algList, setAlgList] = useState<string[]>([]);
+    const [alg, setAlg] = useState<string>("");
+    const [n, setN] = useState<string>("");
+    const [sortData, setSortData] = useState<number[][]>([]);
     const [step, setStep] = useState<number>(0);
-    const [stepData, setStepData] = useState<number[]>([]);
-    const [controls, setControls] = useState<boolean>(false);
     const [playing, setPlaying] = useState<boolean>(false);
+    const [intervalId, setIntervalId] = useState<NodeJS.Timeout>();
+
+    function lastStep() {
+        if (step > 0) {
+            setStep(step => step - 1);
+        }
+    }
+
+    function nextStep() {
+        if (step < sortData.length - 1) {
+            setStep(step => step + 1);
+        }
+    }
+
+    function handleAlgChange(e: any): void {
+        setAlg(e.value);
+    }
+
+    function handleNChange(e: any): void {
+        const val = e.target.value;
+
+        if (isNaN(val) && val !== "") {
+            return;
+        }
+
+        setN(val);
+    }
+
+    function handleSubmitClick(): void {
+        if (alg === "" || n === "") {
+            return;
+        }
+
+        fetch(`${API_URL}/sort/${alg}/${n}`)
+            .then(res => res.json())
+            .then(data => setSortData(data));
+    }
+
+    function handlePrevClick(): void {
+        lastStep();
+    }
+
+    function handleNextClick(): void {
+        nextStep();
+    }
+
+    function handlePauseClick(): void {
+        setPlaying(false);
+    }
+
+    function handlePlayClick(): void {
+        setPlaying(true);
+    }
 
     useEffect(() => {
-        if (data.length > 0) {
-            setStep(0);
-            setStepData(data[0]);
-            setControls(true);
-            setPlaying(false);
-        }
-    }, [data]);
+        fetch(`${API_URL}/list`)
+            .then(res => res.json())
+            .then(data => setAlgList(data));
+    }, []);
 
     useEffect(() => {
-        if (step < 0) {
-            setStep(0);
-        } else if (step > data.length) {
-            setStep(data.length-1);
-        } else if (data.length > 0) {
-            setStepData(data[step]);
+        clearInterval(intervalId);
+
+        if (playing) {
+            setIntervalId(setInterval(() => {
+                nextStep();
+            }, 10));
         }
-    }, [step]);
+
+        return () => clearInterval(intervalId);
+    }, [playing]);
 
     return (
         <div className="app">
-            <Visualizer playing={playing} stepData={stepData} />
-            <ControlBar setData={setData} setPlaying={setPlaying} setStep={setStep} controls={controls} playing={playing} step={step} />
+            <div className="visualizer">
+                {sortData.length > 0 && step < sortData.length &&
+                    sortData[step].map(num => (
+                        <div className="data-bar" key={num} style={{height: num}} />
+                    )
+                )}
+            </div>
+            <div className="algorithm-form">
+                <div className="algorithm-select">
+                    <Select placeholder="Select algorithm" options={algList.map(x => ({value: x, label: x}))} onChange={handleAlgChange} />
+                </div>
+                <div className="algorithm-n">
+                    <input type="text" placeholder="n" value={n} onChange={handleNChange} />
+                </div>
+                <div className="algorithm-submit">
+                    <button onClick={handleSubmitClick}>Submit</button>
+                </div>
+            </div>
+            <div className="control-bar">
+                <div className="control-bar-prev">
+                    <button onClick={handlePrevClick}>Prev</button>
+                </div>
+                {playing ?
+                    <div className="control-bar-pause">
+                        <button onClick={handlePauseClick}>Pause</button>
+                    </div>
+                :
+                    <div className="control-bar-play">
+                        <button onClick={handlePlayClick}>Play</button>
+                    </div>
+                }
+                <div className="control-bar-next">
+                    <button onClick={handleNextClick}>Next</button>
+                </div>
+            </div>
         </div>
     );
 }
